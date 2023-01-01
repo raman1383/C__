6e9220b480859d25837576698f077c2daa5cc4ab
raman1383 <raman83shariati@gmail.com>
2023-01-01 18:07:27 +0330

    k

--- main.cpp
+++ main.cpp
@@ -1,74 +1,19 @@
-// C++ program to find the next optimal move for
-// a player
-
 #include <iostream>
-#include "minimax.hh"
 
-#include <string>
 
-//Part A: define a primary template
-template<class T>
-T app_max(T a, T b) { return (a > b ? a : b); }
-
-//Part B: explicit specialization for T=std::string,
-template<>
-std::string app_max<std::string>(std::string a, std::string b) {
-    return (a[0] > b[0] ? a : b);
+constexpr uint32_t f2(const uint32_t n) {
+    return (n <= 1) ? 1 : n * f2(n - 1);
 }
-//part C: test function
 
-
-template<typename T>
-struct X {
-    X(T init) : m(init) {}
-
-    T increase() { return ++m; }
-
-    T m;
-};
-
-template<> //Note: no parameters inside <>, it tells compiler
-//"hi i am a fully specialized template"
-struct X<char> { //Note: <char> after X, tells compiler
-// "Hi, this is specialized only for type char"
-    X(char init) : m(init) {}
-
-    char increase() { return (m < 127) ? ++m : (m = -128); }
-
-    char toUpperCase() {
-        if ((m >= 'a') && (m <= 'z')) m += 'A' - 'a';
-        return m;
-    }
-
-    char m;
-};
-
+uint32_t f1(const uint32_t n) {
+    return (n <= 1) ? 1 : n * f2(n - 1);
+}
 
 int main() {
-//    char board[3][3] =
-//            {
-//                    {'x', 'o', 'x'},
-//                    {'o', 'o', 'x'},
-//                    {'_', '_', '_'}
-//            };
-//
-//    Move bestMove = findBestMove(board);
-//
-//    printf("The Optimal Move is :\n");
-//    printf("ROW: %d COL: %d\n\n", bestMove.row,
-//           bestMove.col);
 
-//    string a = "abc", b="efg";
-//    cout << app_max<int>(5, 6) << endl; //line A
-//    cout << app_max<string>(a, b) << endl; //line B
-//
-//    string x="abc", y="efg";
-//    cout << app_max(x,y)<<endl;
-
-    X<int> x1(5); //line A
-    std::cout << x1.increase() << std::endl;
-    X<char> x2('b'); //line B
-    std::cout << x2.toUpperCase() << std::endl;
-
+    uint32_t a1 = f1(10); //run-time computation
+    uint32_t a2 = f2(10); //run-time computation
+    const uint32_t a3 = f2(10); //compile-time computation
+    std::cout << "a1=" << a1 << ", a2=" << a2 << std::endl;
     return 0;
 }
