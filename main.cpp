18cc216f14b2ece887171dc05c13c01bde2a39ec
raman1383 <raman83shariati@gmail.com>
2023-01-02 07:45:05 +0330

    k2

--- main.cpp
+++ main.cpp
@@ -15,21 +15,7 @@ using namespace std;
 
 int main() {
 
-//    vector<int> vec;
-//    vec.push_back(1);
-//    vec.push_back(2);
-//    vec.push_back(3);
-//    for (const auto elem: vec) {
-//        std::cout << elem << ", ";
-//    }
-//
-//    std::cout << vec.at(2);
-//// is the same as
-//    std::cout << vec[2];
-//// which is the same as
-//    try {
-//        vec.at(999999);
-//    } catch (std::out_of_range& e) { cout << e.what();}
+
 
     std::list<double> lst{1.1, 2.2, 3.3, 4.2};
     for (auto& elem : lst) {
