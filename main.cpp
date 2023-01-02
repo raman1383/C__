e956006a76e8d47eb1506dd0e2cdfc7388d97d0a
raman1383 <raman83shariati@gmail.com>
2023-01-02 23:15:17 +0330

    k2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <list>
-//#include <chrono>
 
 using namespace std;
 
+int step_func(int y) {
+    return y > 0 ? 1 : 0;
+}
 
-//struct email {
-//    string subject;
-//    string body;
-//    string from;
-////    std::chrono::time_point datetime;
-//};
 
 int main() {
 
+    vector<int> vec{1, 3, 5, 6, 8, 54, 99};
+    cout << boolalpha << binary_search(vec.begin(), vec.end(), 4) << endl;
 
+    vector<int> vec2{2, 3, 11, 6, 8, 4, 9};
+    sort(vec2.begin(), vec2.end());
+    cout << vec2[2] << endl;
 
-    std::list<double> lst{1.1, 2.2, 3.3, 4.2};
-    for (auto& elem : lst) {
-        std::cout << elem<<", ";
-    }
+    cout<< step_func(-32);
 
     return 0;
 }
